b9e07ed6125256b6d18a5e39d74fe5f263e147c4
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-07 18:37:16 -0300

    feat(git): making use of the usePost Hook

---
+++ web/src/usePost.ts
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+export default function usePost() {
+  const [erro, setErro] = useState('');
+  const [success, setSuccess] = useState(false);
+
+  async function registerData<T>({ url, dados }:
+    { url: string, dados: T }) {
+    try {
+      await fetch(`http://localhost:8080/${url}`, {
+        method: 'POST',
+        body: JSON.stringify(dados)
+      })
+      setSuccess(true);
+    } catch (erro) {
+      setErro('Não foi possível enviar os dados');
+    }
+  }
+
+  return { registerData, success, erro }
+}
